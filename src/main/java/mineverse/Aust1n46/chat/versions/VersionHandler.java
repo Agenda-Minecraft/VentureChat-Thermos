c8d5479c1d853bd065916a4396431af373eb8932
Kiwi <me@kiwi.cat>
2023-10-16 07:39:55 +0800

    Thermos 1.7.10 support

--- src/main/java/mineverse/Aust1n46/chat/versions/VersionHandler.java
+++ src/main/java/mineverse/Aust1n46/chat/versions/VersionHandler.java
@@ -4,74 +4,90 @@ import com.comphenix.protocol.utility.MinecraftVersion;
 
 public final class VersionHandler {
 
-	public static final MinecraftVersion SERVER_VERSION = MinecraftVersion.getCurrentVersion();
-	private static final MinecraftVersion MC1_19 = new MinecraftVersion(1, 19, 0);
-	private static final MinecraftVersion MC1_19_1 = new MinecraftVersion(1, 19, 1);
+    public static MinecraftVersion serverVersion() {
+        try {
+            return MinecraftVersion.getCurrentVersion();
+        } catch (NoSuchMethodError e) {
+			return MinecraftVersion.SKIN_UPDATE;
+        }
+    }
 
-	private VersionHandler() {
-	}
 
-	public static boolean is1_7() {
-		return SERVER_VERSION.getMinor() == 7 && SERVER_VERSION.getMajor() == 1;
-	}
+    private static final MinecraftVersion MC1_19 = new MinecraftVersion(1, 19, 0);
+    private static final MinecraftVersion MC1_19_1 = new MinecraftVersion(1, 19, 1);
 
-	public static boolean is1_8() {
-		return SERVER_VERSION.getMinor() == 8 && SERVER_VERSION.getMajor() == 1;
-	}
+    private VersionHandler() {
+    }
 
-	public static boolean is1_9() {
-		return SERVER_VERSION.getMinor() == 9 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_7() {
+        return serverVersion().getMinor() == 7 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_10() {
-		return SERVER_VERSION.getMinor() == 10 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_8() {
+        return serverVersion().getMinor() == 8 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_11() {
-		return SERVER_VERSION.getMinor() == 11 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_9() {
+        return serverVersion().getMinor() == 9 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_12() {
-		return SERVER_VERSION.getMinor() == 12 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_10() {
+        return serverVersion().getMinor() == 10 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_13() {
-		return SERVER_VERSION.getMinor() == 13 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_11() {
+        return serverVersion().getMinor() == 11 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_14() {
-		return SERVER_VERSION.getBuild() != 4 && SERVER_VERSION.getMinor() == 14 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_12() {
+        return serverVersion().getMinor() == 12 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_14_4() {
-		return SERVER_VERSION.getBuild() == 4 && SERVER_VERSION.getMinor() == 14 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_13() {
+        return serverVersion().getMinor() == 13 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_15() {
-		return SERVER_VERSION.getMinor() == 15 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_14() {
+        return serverVersion().getBuild() != 4 && serverVersion().getMinor() == 14 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_16() {
-		return SERVER_VERSION.getMinor() == 16 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_14_4() {
+        return serverVersion().getBuild() == 4 && serverVersion().getMinor() == 14 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_17() {
-		return SERVER_VERSION.getMinor() == 17 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_15() {
+        return serverVersion().getMinor() == 15 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_18() {
-		return SERVER_VERSION.getMinor() == 18 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_16() {
+        return serverVersion().getMinor() == 16 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean is1_19() {
-		return SERVER_VERSION.getBuild() == 0 && SERVER_VERSION.getMinor() == 19 && SERVER_VERSION.getMajor() == 1;
-	}
+    public static boolean is1_17() {
+        return serverVersion().getMinor() == 17 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean isUnder_1_19() {
-		return !SERVER_VERSION.isAtLeast(MC1_19);
-	}
+    public static boolean is1_18() {
+        return serverVersion().getMinor() == 18 && serverVersion().getMajor() == 1;
+    }
 
-	public static boolean isAbove_1_19() {
-		return SERVER_VERSION.isAtLeast(MC1_19_1);
-	}
+    public static boolean is1_19() {
+        return serverVersion().getBuild() == 0 && serverVersion().getMinor() == 19 && serverVersion().getMajor() == 1;
+    }
+
+    public static boolean isUnder_1_19() {
+        try {
+            return !serverVersion().isAtLeast(MC1_19);
+        } catch (NoSuchMethodError e) {
+            return true;
+        }
+    }
+
+    public static boolean isAbove_1_19() {
+        try {
+            return serverVersion().isAtLeast(MC1_19_1);
+        } catch (NoSuchMethodError e) {
+            return false;
+        }
+    }
 }
